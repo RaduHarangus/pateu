283ad0b242dca92ac53c1f994987e29fe92447d0
Radu H <52208792+Radoogle@users.noreply.github.com>
2021-08-04 09:22:14 +0300

    task finished 2

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat: {
+            main: {
+                src: [
+                    'js/nav.js',
+                    'js/subscribe.js',
+                    'js/timer.js',
+                    'js/carousel.js',
+                    'js/contact.js',
+                    'js/tooltips.js',
+                ],
+                dest: 'main.js',
+                options: {
+                    separator: '\n\n',
+                    interrupt: true
+                }
+            }
+        },
+        less: {
+            files: {
+                src: [
+                    'less/main.less',
+                ],
+                dest: 'style.css'
+            }
+        },
+        watch: {
+            less: {
+                files: [
+                    'less/*.less',
+                    'less/**/*.less'
+                ],
+                tasks: ['less']
+            },
+            js: {
+                files: [
+                    'js/*.js'
+                ],
+                tasks: ['concat']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // the default task can be run just by typing "grunt" on the command line
+    grunt.registerTask('default', ['watch', 'less', 'concat']);
+}
